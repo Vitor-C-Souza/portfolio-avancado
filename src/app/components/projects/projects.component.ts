d73b1a4a8a06a34463a7559883b39b45b9cf7ddd
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-02-27 10:44:24 -0300

    Ajustado o site para facilitar mais a leitura

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -25,5 +25,52 @@ export class ProjectsComponent {
       link: 'https://github.com/Vitor-C-Souza/catalagos-de-livros',
       tecnologies: ['Java', 'Spring Boot', 'MongoDB', 'GraphQL'],
     },
+    {
+      title: 'blogpessoal API',
+      description:
+        'This API is designed to manage a personal blog, allowing the author to create, edit and publish posts, while readers can navigate the content, comment and interact.',
+      link: 'https://github.com/Vitor-C-Souza/blogpessoal',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Rest API'],
+    },
+    {
+      title: 'bytebank API',
+      description: 'Project for Bytebank, data processor of a file.csv',
+      link: 'https://github.com/Vitor-C-Souza/bytebank',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Spring Batch'],
+    },
+    {
+      title: 'Star Wars API',
+      description:
+        'Creation, management and visualization of a Star Wars -inspired galaxy, including planets, star systems, characters and spaceships.',
+      link: 'https://github.com/Vitor-C-Souza/star-wars-api',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Rest API'],
+    },
+    {
+      title: 'Jornada Milhas API',
+      description: 'API to return testimonials and travel destinations.',
+      link: 'https://github.com/Vitor-C-Souza/jornada-milhas-api',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Rest API'],
+    },
+    {
+      title: 'Banco Digital',
+      description:
+        'This API is designed to manage the features of a digital bank, allowing users to create accounts, perform transactions, track balances, manage cards and control their finances in a practical and safe way.',
+      link: 'https://github.com/Vitor-C-Souza/banco-digital',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Rest API'],
+    },
+    {
+      title: 'Controle de orcamento familiar API',
+      description:
+        'Family budget control is an application that aims to provide a simple, efficient and intuitive way for families to manage their finances. This platform allows users to monitor their revenues, expenses and economies, helping to plan the financial future and achieve savings goals in an organized and automated manner.',
+      link: 'https://github.com/Vitor-C-Souza/controle-de-orcamento-familiar-api',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'RestAPI'],
+    },
+    {
+      title: 'Aluraflix API',
+      description:
+        'Challenge proposed for Alura to Backend to create an API for Aluraflix website',
+      link: 'https://github.com/Vitor-C-Souza/aluraflix-api',
+      tecnologies: ['Java', 'Spring Boot', 'MySQL', 'Rest API'],
+    },
   ];
 }
