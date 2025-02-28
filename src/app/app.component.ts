c9a24b49b08302aba0397e79e88175109561790c
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-02-28 12:54:06 -0300

    Desenvolvido a pagina contact mas o serviço não esta funcionando ainda, falta o backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/page/contact/contact.component.ts
+++ src/app/page/contact/contact.component.ts
@@ -1,11 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule, ContainerComponent],
+  imports: [CommonModule, ContainerComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: [''],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      alert('Mensagem enviada com sucesso! Entrarei em contato em breve.');
+      this.contactForm.reset();
+    } else {
+      alert('Preencha todos os campos obrigatórios!');
+    }
+  }
+}
