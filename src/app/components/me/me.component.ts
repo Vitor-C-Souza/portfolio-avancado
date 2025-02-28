09ec0a95bee3a10524b5e3a54e3839743c2a8312
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-02-28 12:19:54 -0300

    adicionando funcionalidade ao botão contact me

--- src/app/components/me/me.component.ts
+++ src/app/components/me/me.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-me',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss',
 })
